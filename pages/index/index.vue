9d2f2f71a907a712fb60bf367d0d5f111255b4f5
zyj <975023831@qq.com>
2024-12-07 18:31:32 +0800

    init

---
+++ pages/index/index.vue
@@ -0,0 +1,54 @@
+<template>
+	<view class="content">
+		<iliyaCapsuleToys :giftImgList="giftImgList" :disabled="disabled" :resLv="resLv" :giftRes="giftRes"
+			:resBallImg="resBallImg" @start="start" @canNotStart="canNotStart" @ok="ok" />
+	</view>
+</template>
+
+<script>
+	import iliyaCapsuleToys from '../../components/iliya-capsule-toys/iliya-capsule-toys.vue';
+	export default {
+		components: {
+			iliyaCapsuleToys
+		},
+		data() {
+			return {
+				giftRes: {
+					giftName: null,
+					url: ""
+				},
+				giftImgList: [],
+				resBallImg: '',
+				disabled: false,
+				resLv: 1
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			start(fn) {
+				this.giftRes.giftName = '预制菜'
+				this.giftRes.url = '../../static/zi-qiu.png'
+				fn()
+			},
+			canNotStart() {
+				uni.showModal({
+					title: '提示',
+					content: '余额不足'
+				})
+			},
+			ok() {
+				console.log('确认中奖信息');
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		padding-top: 200rpx;
+		background: red;
+		height: 100vh;
+	}
+</style>
